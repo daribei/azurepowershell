e31d40cd400e0529c1e8a3c47b08e2b0a659d90e
Daniel Ribeiro <34457162+xdanielribeiro@users.noreply.github.com>
2019-03-04 11:32:52 -0300

    Update CreateAzureStorageAccountAndGetStorageKey.ps1

--- CreateAzureStorageAccountAndGetStorageKey.ps1
+++ CreateAzureStorageAccountAndGetStorageKey.ps1
@@ -1,3 +1,17 @@
+
+<#
+
+.DESCRIPTION
+
+This sample demonstrates how to create an Azure Storage Account and get the storage key.
+
+.NOTES
+
+1. Before you use this sample, please install the latest version of Azure PowerShell from here: http://go.microsoft.com/?linkid=9811175&clcid=0x409
+2. Provide the appropriate values for each variable.
+
+#>
+
 # Autenticação
 Login-AzureRmAccount
 
