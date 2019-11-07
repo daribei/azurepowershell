fcbd9c45e596d3efafab25fcbe53dbe483f81951
Daniel Ribeiro <34457162+xdanielribeiro@users.noreply.github.com>
2019-11-07 14:01:23 -0300

    Add files via upload

---
+++ ExportAzResourcesToCSV.ps1
@@ -0,0 +1,16 @@
+﻿#Export Azure Resources to CSV files with PowerShell
+
+Login-AzAccount
+
+Select-AzSubscription -Subscription <Subscription Name / ID>
+
+$Resources = Get-AzResource
+$ResourcesOutput = $Resources | ForEach-Object { 
+    [PSCustomObject]@{
+        "ResourceName" = $_.Name
+        "Resource Group" = $_.ResourceGroupName
+        "Resource Type" = $_.ResourceType
+        "Location" = $_.Location
+    }
+}
+$ResourcesOutput | export-csv C:\temp\data.csv -delimiter ";" -force -notypeinformation
